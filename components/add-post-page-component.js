36d6e196b342085c7ca96177385b9bd3c391b845
Fnami51 <fnm5160@gmail.com>
2024-04-03 19:49:13 +0300

    v1.01 - Setting tasks

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -1,6 +1,8 @@
 export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
   const render = () => {
     // TODO: Реализовать страницу добавления поста
+
+    // >> [FNM: Task 2.1] - Скопировать HTML у конкурентов
     const appHtml = `
     <div class="page-container">
       <div class="header-container"></div>
@@ -13,8 +15,13 @@ export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
 
     document.getElementById("add-button").addEventListener("click", () => {
       onAddPostClick({
+        // >> [FNM: Task 2.2] - Закидывать сюда поле_ввода.value
         description: "Описание картинки",
+
+        // >> [FNM: Task 2.3] - imageUr вставлять ссылку использую upload-image-component
         imageUrl: "https://image.png",
+
+        // >> [FNM: Task 2.4] - Отправлять в АПИ (Создать запрос в АПИ "POST" отправлять эти значения)
       });
     });
   };
